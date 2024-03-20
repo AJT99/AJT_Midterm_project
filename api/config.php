d2731dcade5e95bc246752a786c28853220dbe34
Enki314 <enkigenetics@gmail.com>
2024-03-20 17:19:29 +0100

    changed config file

--- api/config.php
+++ api/config.php
@@ -1,18 +1,15 @@
 <?php
-// Retrieve database connection details from environment variables
 $db_host = getenv('DATABASE_HOST');
 $db_port = getenv('DATABASE_PORT');
 $db_name = getenv('DATABASE_NAME');
 $db_user = getenv('DATABASE_USERNAME');
 $db_pass = getenv('DATABASE_PASSWORD');
 
-// Establish database connection
-$conn = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_pass);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . pg_last_error());
+try {
+    $conn = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
-
-echo "Connected successfully";
 ?>
