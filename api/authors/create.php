443d9db7097cace293bb4304b1acfd824a4f32b3
Enki314 <enkigenetics@gmail.com>
2024-03-20 21:00:01 +0100

    modified functionality

--- api/authors/create.php
+++ api/authors/create.php
@@ -1,6 +1,9 @@
 <?php
 include_once '../config.php';
 
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $data = json_decode(file_get_contents("php://input"));
 
@@ -20,8 +23,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo json_encode(array("message" => "Database error: " . $e->getMessage()));
         }
     } else {
-        echo json_encode(array("message" => "Missing required data."));
+        echo json_encode(array("message" => "Missing Required Parameters"));
     }
+} elseif ($_SERVER["REQUEST_METHOD"] == "GET" && $_SERVER["REQUEST_URI"] == "/authors/") {
+    $query = "SELECT id, author FROM authors";
+    $stmt = $conn->query($query);
+    $authors = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($authors);
 } else {
     echo json_encode(array("message" => "Method Not Allowed"));
 }
