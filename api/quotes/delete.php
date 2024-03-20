715a9d55a1a0301d761e2cf022252f00d444eaac
Enki314 <enkigenetics@gmail.com>
2024-03-20 22:10:43 +0100

    modified functionality

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -7,31 +7,19 @@ header('Content-Type: application/json');
 if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
     if (isset($_GET['id'])) {
         $quote_id = $_GET['id'];
+
         $query = "DELETE FROM quotes WHERE id = :id";
         $stmt = $conn->prepare($query);
         $stmt->bindParam(':id', $quote_id);
-        
-        if ($stmt->execute()) {
-            if ($stmt->rowCount() > 0) {
-                echo json_encode(array("message" => "Quote deleted successfully."));
-            } else {
-                echo json_encode(array("message" => "Quote not found."));
-            }
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            echo json_encode(array("message" => "Quote deleted successfully."));
         } else {
-            echo json_encode(array("message" => "Unable to delete quote."));
+            echo json_encode(array("message" => "No Quotes Found"));
         }
     } else {
         echo json_encode(array("message" => "Quote ID parameter is missing."));
-    }
-} elseif ($_SERVER["REQUEST_METHOD"] == "GET" && $_SERVER["REQUEST_URI"] == "/quotes/") {
-    $query = "SELECT id FROM quotes";
-    $stmt = $conn->query($query);
-    $quotes = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    if (count($quotes) > 0) {
-        echo json_encode($quotes);
-    } else {
-        echo json_encode(array("message" => "No Quotes Found"));
     }
 } else {
     echo json_encode(array("message" => "Method Not Allowed"));
