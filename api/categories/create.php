55f2a5e74b570e32c5845bed8a0a407838057631
Enki314 <enkigenetics@gmail.com>
2024-03-20 21:03:07 +0100

    modified functionality

--- api/categories/create.php
+++ api/categories/create.php
@@ -1,27 +1,36 @@
 <?php
 include_once '../config.php';
+
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $data = json_decode(file_get_contents("php://input"));
 
     if (!empty($data->category)) {
-        $query = "INSERT INTO categories (category) VALUES (:category)";
-        $stmt = $conn->prepare($query);
-        $stmt->bindParam(':category', $data->category);
+        try {
+            $query = "INSERT INTO categories (category) VALUES (:category)";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':category', $data->category);
 
-        if ($stmt->execute()) {
-            $category_id = $conn->lastInsertId();
-
-            echo json_encode(array("message" => "Category created successfully.", "category_id" => $category_id));
-        } else {
-            echo json_encode(array("message" => "Unable to create category."));
+            if ($stmt->execute()) {
+                $category_id = $conn->lastInsertId();
+                echo json_encode(array("message" => "Category created successfully.", "category_id" => $category_id));
+            } else {
+                echo json_encode(array("message" => "Unable to create category."));
+            }
+        } catch (PDOException $e) {
+            echo json_encode(array("message" => "Database error: " . $e->getMessage()));
         }
     } else {
-        echo json_encode(array("message" => "Missing required data."));
+        echo json_encode(array("message" => "Missing Required Parameters"));
     }
+} elseif ($_SERVER["REQUEST_METHOD"] == "GET" && $_SERVER["REQUEST_URI"] == "/categories/") {
+    $query = "SELECT id, category FROM categories";
+    $stmt = $conn->query($query);
+    $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($categories);
 } else {
     echo json_encode(array("message" => "Method Not Allowed"));
 }
-
 ?>
